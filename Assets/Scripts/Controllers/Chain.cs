eb61b1a3623cf25ab78eace0987b1339795830af
Pansimula <mattstger@gmail.com>
2025-01-27 13:31:38 -0500

    Setup of public links of main scripts

---
+++ Assets/Scripts/Controllers/Chain.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TerrainSystem;
+
+namespace Controllers
+{
+    public class Chain : MonoBehaviour
+    {
+        public EndPoint start;
+        public EndPoint end;
+    }
+}
---
+++ Assets/Scripts/Controllers/ControlsUI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers
+{
+    public class ControlsUI : MonoBehaviour
+    {
+        public GridLayoutGroup singleGrid;
+        public GridLayoutGroup groupGrid;
+        public GameObject hotkeyUIPrefab;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
